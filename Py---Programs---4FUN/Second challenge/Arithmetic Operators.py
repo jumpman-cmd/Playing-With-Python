1dfd891f6616c559b19ceb1170f28e922efee913
jumpman-cmd <codeks7407@gmail.com>
2024-06-06 23:33:56 +0200

    Initial Commit

---
+++ Py---Programs---4FUN/Second challenge/Arithmetic Operators.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = int(input())
+
+# Perform the required operations
+sum_result = a + b
+difference_result = a - b
+product_result = a * b
+
+# Print the results
+print(sum_result)
+print(difference_result)
+print(product_result)
